e6f890f88e753e4104319e3753f2743543062361
Robert Giles <robert.ryan.giles@gmail.com>
2022-03-31 23:03:50 -0500

    Video 22

--- Video_22/Program.vb
+++ Video_22/Program.vb
@@ -2,6 +2,13 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-
+        'Video 22 - Replacing Substrings
+        Dim myString As String = Nothing
+        Dim finalString As String = Nothing
+        Console.WriteLine("Please enter a string: ")
+        myString = Console.ReadLine()
+        finalString = myString.Replace("hello", "fart")
+        Console.WriteLine(finalString)
+        Console.ReadLine()
     End Sub
 End Module
