7d42f727de449792fe2c5e576992fa3c85be3de9
Robert Giles <robert.ryan.giles@gmail.com>
2022-03-31 23:08:11 -0500

    Video 23

--- Video_22/Program.vb
+++ Video_22/Program.vb
@@ -3,12 +3,21 @@ Imports System
 Module Program
     Sub Main(args As String())
         'Video 22 - Replacing Substrings
-        Dim myString As String = Nothing
-        Dim finalString As String = Nothing
+        'Dim myString As String = Nothing
+        'Dim finalString As String = Nothing
+        'Console.WriteLine("Please enter a string: ")
+        'myString = Console.ReadLine()
+        'finalString = myString.Replace("hello", "fart")
+        'Console.WriteLine(finalString)
+        'Console.ReadLine()
+
+        'Video 23 - Comparing Strings
+        Dim userString As String = Nothing
+        Dim compString As String = "donk bonk"
         Console.WriteLine("Please enter a string: ")
-        myString = Console.ReadLine()
-        finalString = myString.Replace("hello", "fart")
-        Console.WriteLine(finalString)
+        userString = Console.ReadLine()
+        Console.WriteLine(String.Compare(userString, compString))
         Console.ReadLine()
+
     End Sub
 End Module
