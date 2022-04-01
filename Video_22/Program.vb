e4b6fca527411def8748320b30bca8264295f92b
Robert Giles <robert.ryan.giles@gmail.com>
2022-03-31 23:12:48 -0500

    Video 24

--- Video_22/Program.vb
+++ Video_22/Program.vb
@@ -12,12 +12,25 @@ Module Program
         'Console.ReadLine()
 
         'Video 23 - Comparing Strings
-        Dim userString As String = Nothing
-        Dim compString As String = "donk bonk"
-        Console.WriteLine("Please enter a string: ")
-        userString = Console.ReadLine()
-        Console.WriteLine(String.Compare(userString, compString))
-        Console.ReadLine()
+        'Dim userString As String = Nothing
+        'Dim compString As String = "donk bonk"
+        'Console.WriteLine("Please enter a string: ")
+        'userString = Console.ReadLine()
+        'Console.WriteLine(String.Compare(userString, compString))
+        'Console.ReadLine()
 
+        'Video 24 - Select Case
+        Dim userInt As String = Nothing
+        Console.WriteLine("Please enter an integer: ")
+        userInt = Console.ReadLine()
+        Select Case userInt
+            Case 0
+                Console.WriteLine("Hello")
+            Case 1
+                Console.WriteLine("Ayyy")
+            Case 2
+                Console.WriteLine("OOOK")
+        End Select
+        Console.ReadLine()
     End Sub
 End Module
